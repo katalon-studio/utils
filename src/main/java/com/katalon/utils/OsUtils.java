8c9723213e43bdfb63f40749824931beafc4798d
Alex Smith <dev.alex.88@gmail.com>
2019-04-08 14:43:45 +0700

    refactor

--- src/main/java/com/katalon/utils/OsUtils.java
+++ src/main/java/com/katalon/utils/OsUtils.java
@@ -95,8 +95,9 @@ class OsUtils {
 
     static String[] getEnvironmentVariables() {
         // Refer to https://docs.oracle.com/javase/7/docs/api/java/lang/Runtime.html#exec(java.lang.String[],%20java.lang.String[],%20java.io.File)
-        return System.getenv().entrySet().stream().map(entry -> {
-            return entry.getKey()+ "=" + entry.getValue();
-        }).collect(Collectors.toList()).toArray(new String[]{});
+        return System.getenv().entrySet().stream()
+                .map(entry -> entry.getKey()+ "=" + entry.getValue())
+                .collect(Collectors.toList())
+                .toArray(new String[]{});
     }
 }
